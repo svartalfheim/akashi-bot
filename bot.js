9da289a320043cdc2a975bb6c82085da7fecb8d2
YusukeTashiro <svartalf@niko.local>
2016-03-03 23:44:13 +0900

    rename method and bugfix

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ var redisStorage = redis({
     auth_pass: redisURL.auth.split(":")[1]
 });
 
+var ROOM_CHANNEL_ID = "C0EA7KEMV";
 
 var controller = Botkit.slackbot({
     debug: true,
@@ -23,33 +24,36 @@ var bot = controller.spawn({
     token: process.env.SLACK_TOKEN
 }).startRTM();
 
-var morningGreeting = function(){
+var sayMorningGreeting = function(){
   bot.say({
     text:"おはようございます！",
-    channel:"C0EA7KEMV"
+    channel:ROOM_CHANNEL_ID
   });
 };
 
-var weather = function(){
+var sayTodayWeather = function(){
   request('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010', function (error, response, body){
     if (!error && response.statusCode == 200) {
       var json = JSON.parse(body);
       var today = json.forecasts[0];
       bot.say({
-        text:"今日の天気は"+today.telop+" 最高気温:"+today.temperature.max+"/最低気温:"+today.temperature.min+" ですよ",
-        channel: "C0EA7KEMV"
+        text:"今日の天気は"+today.telop+" 最高気温:"+(today.temperature.max.celsius || '-')+"/最低気温:"+(today.temperature.min.celsius || '-')+" ですよ",
+        channel: ROOM_CHANNEL_ID
       });
     }
   });
 };
 
 controller.hears(['今日の天気は？'],'direct_message,direct_mention,mention',function(bot, message) {
-  weather();
+  sayTodayWeather();
 });
 
-new CronJob('0 0 10 * * 1-5', function(){
-  morningGreeting();
+new CronJob('0 0 8 * * 1-5', function(){
+  sayMorningGreeting();
+  sayTodayWeather();
 }, null, true, 'Asia/Tokyo');
+
+
 
 // To keep Heroku's free dyno awake
 http.createServer(function(request, response) {
