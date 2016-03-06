83570400367c8e989a88f76fc506a1a76d463133
YusukeTashiro <svartalf@niko.local>
2016-03-06 20:46:33 +0900

    add arsenal

---
+++ modules/arsenal.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var fs = require("fs");
+var _ = require("lodash");
+var config = require('../config');
+
+module.exports = {
+  fetch:function(callback){
+    var data = fs.readFileSync("../assets/arsenal.json");
+    var json = JSON.parse(data);
+    var arsenals = [];
+    var weekday = new Date().getDay();
+
+    var _.forEach(json,function(arsenal){
+      var ships = [];
+      _.forEach(arsenal.second_ships,function(ship){
+        if(_.indexOf(ship.weekdays,weekday) != -1){
+          ships.push(ship);
+        }
+      });
+
+      if(ships.length > 0){
+        arsenals.push({
+          "name":arsenal.name,
+          "second_ships":ships
+        })
+      }
+    });
+
+    callback(arsenals);
+  },
+  sayTodayArsenal:function(bot){
+    this.fetch(function(arsenals){
+      arsenal.fetch(function(arsenals){
+        var text = '今日の改修は\n';
+        _.forEach(arsenals,function(arsenal){
+          text += arsenal.name + ' 二番艦(';
+          _.forEach(arsenal.second_ships,function(ship){
+            text += ship.name + ' ';
+          });
+          text += ')\n';
+        });
+        text += 'ですね！';
+      });
+
+      bot.say({
+        text:text,
+        channel: config.CHANNEL_ID.ROOM
+      });
+    });
+  }
+};
---
+++ plugins/arsenal.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var _ = require("lodash");
+var arsenal = require('../modules/arsenal');
+
+module.exports = function(controller,bot){
+
+  controller.hears(['今日の改修は？'],'direct_message,direct_mention,mention',function(message) {
+    arsenal.fetch(bot);
+  });
+};
