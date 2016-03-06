c7e94b52795fc9b5e3bea6065132fb4b0be13061
YusukeTashiro <svartalf@niko.local>
2016-03-06 21:30:38 +0900

    fix rename method

--- plugins/weather.js
+++ plugins/weather.js
@@ -4,6 +4,6 @@ var weather = require('../modules/weather');
 
 module.exports = function(controller,bot){
   controller.hears(['今日の天気は？'],'direct_message,direct_mention,mention',function(bot, message) {
-    weather.sayTodayArsenal(bot);
+    weather.sayTokyo(bot);
   });
 };
