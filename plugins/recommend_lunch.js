fe6a54f38cf5506688ab0c48388cc1c58f998cf9
YusukeTashiro <svartalf@niko.local>
2016-03-09 01:18:22 +0900

    mofidy reply

--- plugins/recommend_lunch.js
+++ plugins/recommend_lunch.js
@@ -7,15 +7,12 @@ module.exports = function(controller,bot){
     return items[Math.floor(Math.random() * items.length)];
   };
 
-  controller.hears(['お(腹|なか)すいた'],['message_received'],function(message) {
+  controller.hears(['お(腹|なか)すいた'],['message_received'],function(bot,message) {
     var menus = [
       "カレー","そば","うどん","ステーキ","パスタ","寿司","中華","ラーメン","とんかつ",
       "親子丼","牛丼","定食","ピザ","ハンバーグ","焼魚","唐揚げ"
     ];
 
-    bot.say({
-      text:random(menus)+'とかどうですか？',
-      channel:config.CHANNEL_ID.ROOM
-    });
+    bot.reply(message,random(menus)+'とかどうですか？');
   });
 };
