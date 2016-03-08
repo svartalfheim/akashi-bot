fcd711a54bbff1eebe1f0aa23c704e64a60918be
YusukeTashiro <svartalf@niko.local>
2016-03-09 01:26:06 +0900

    add debug code

--- plugins/recommend_lunch.js
+++ plugins/recommend_lunch.js
@@ -7,7 +7,8 @@ module.exports = function(controller,bot){
     return items[Math.floor(Math.random() * items.length)];
   };
 
-  controller.hears(['お(腹|なか)すいた'],['message_received'],function(bot,message) {
+  controller.hears(['お(腹|なか)(す|空)いた'],['message_received'],function(bot,message) {
+    console.log('you are hungry');
     var menus = [
       "カレー","そば","うどん","ステーキ","パスタ","寿司","中華","ラーメン","とんかつ",
       "親子丼","牛丼","定食","ピザ","ハンバーグ","焼魚","唐揚げ"
