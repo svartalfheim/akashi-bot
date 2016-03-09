0b8fcf885f3047f67bf4ae0e48be393014d844e7
YusukeTashiro <svartalf@niko.local>
2016-03-09 22:56:03 +0900

    fix unicode

--- plugins/translate.js
+++ plugins/translate.js
@@ -5,8 +5,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 module.exports = function(controller,bot){
-  controller.hears(['([\x{0430}-\x{044F}\x{0410}-\x{042F} ]+)'],['direct_message', 'ambient'],function(bot,message){
-    var matches = message.text.match(/([\x{0430}-\x{044F}\x{0410}-\x{042F} ]+)/iu);
+  controller.hears(['([0-9\u0430-\u044F\u0410-\u042F ]+)'],['direct_message', 'ambient'],function(bot,message){
+    var matches = message.text.match(/([0-9\u0430-\u044F\u0410-\u042F ]+)/iu);
     var str = matches[1];
     console.log(str);
     request('https://translate.google.co.jp/?hl=ja#ru/ja/'+encodeURIComponent(str),function(error,response,body){
