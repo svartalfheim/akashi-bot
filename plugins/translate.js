34ab477e73305115dd8e48a3d97d0a59c20ca96b
YusukeTashiro <svartalf@niko.local>
2016-03-09 22:50:55 +0900

    fix accesser

--- plugins/translate.js
+++ plugins/translate.js
@@ -5,13 +5,14 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 module.exports = function(controller,bot){
-  controller.hears(['ru2ja (.*)'],['direct_message', 'ambient'],function(bot,message){
-    var matches = message.text.match(/ru2ja (.*)/i);
+  controller.hears(['([\x{0430}-\x{044F}\x{0410}-\x{042F} ]+)'],['direct_message', 'ambient'],function(bot,message){
+    var matches = message.text.match(/([\x{0430}-\x{044F}\x{0410}-\x{042F} ]+)/iu);
     var str = matches[1];
+    console.log(str);
     request('https://translate.google.co.jp/?hl=ja#ru/ja/'+encodeURIComponent(str),function(error,response,body){
       if (response.statusCode == 200) {
         var $ = cheerio.load(body);
-        var jaString = $.find('#result_box span').text();
+        var jaString = $('#result_box span').text();
         bot.reply(message,'「'+jaString+'」って言ってるみたいですよ？');
       }else{
         console.log(error);
