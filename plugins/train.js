f36214c2cd838d869ee0429b946edff01b2ccae9
YusukeTashiro <svartalf@niko.local>
2016-03-16 22:50:12 +0900

    add hears

--- plugins/train.js
+++ plugins/train.js
@@ -11,4 +11,12 @@ module.exports = function(controller,bot){
   controller.hears(['東西線は？'],'direct_message,direct_mention,mention',function(message) {
     train.sayTozaiInfo(bot);
   });
+
+  controller.hears(['中央線は？'],'direct_message,direct_mention,mention',function(message) {
+    train.sayChuoInfo(bot);
+  });
+
+  controller.hears(['西武線は？'],'direct_message,direct_mention,mention',function(message) {
+    train.saySeibuInfo(bot);
+  });
 };
