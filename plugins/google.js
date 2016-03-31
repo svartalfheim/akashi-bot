a658e5cac83659714748e383f9a1dedc65ed1832
YusukeTashiro <svartalfheim.jp@gmail.com>
2016-04-01 01:43:27 +0900

    nullチェック追加

--- plugins/google.js
+++ plugins/google.js
@@ -1,9 +1,11 @@
 module.exports = function(controller) {
     controller.hears(['(調べて) (.*)'], ['direct_message', 'direct_mention', 'mention', 'ambient'], function(bot, message) {
-        var matches = message.text.match(/ggr (.*)/i);
-        var name = matches[1];
-        var res = matches[1].replace(' ', '+');
+        var matches = message.text.match(/調べて (.*)/i);
+        if(matches){
+          var name = matches[1];
+          var res = matches[1].replace(' ', '+');
 
-        bot.reply(message, 'っ [ https://www.google.co.jp/search?q=' + res + ' ]');
+          bot.reply(message, 'っ [ https://www.google.co.jp/search?q=' + res + ' ]');
+        }
     });
 };
