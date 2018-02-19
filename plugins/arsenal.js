36cb41be55fd79381569ccb750a6966ba5d2d7e3
YusukeTashiro <svartalfheim.jp@gmail.com>
2018-02-19 23:19:19 +0900

    eslint周りの修正

--- plugins/arsenal.js
+++ plugins/arsenal.js
@@ -1,14 +1,13 @@
 'use strict'
 
-var _ = require("lodash");
 var arsenal = require('../modules/arsenal');
 
 module.exports = function(controller,bot){
 
-  controller.hears(['今日の改修は？'],'direct_message,direct_mention,mention',function(message) {
+  controller.hears(['今日の改修は？'],'direct_message,direct_mention,mention',function() {
     arsenal.sayTodayArsenal(bot,arsenal.PRIORITY.HIGH);
   });
-  controller.hears(['今日の改修全部見せて'],'direct_message,direct_mention,mention',function(message) {
+  controller.hears(['今日の改修全部見せて'],'direct_message,direct_mention,mention',function() {
     arsenal.sayTodayArsenal(bot,arsenal.PRIORITY.LOW);
   });
 };
--- plugins/recommend_lunch.js
+++ plugins/recommend_lunch.js
@@ -1,14 +1,11 @@
 'use strict'
 
-var config = require('../config');
-
-module.exports = function(controller,bot){
+module.exports = function(controller){
   function random(items) {
     return items[Math.floor(Math.random() * items.length)];
-  };
+  }
 
   controller.hears(['お(腹|なか)(す|空)いた'],['direct_message', 'ambient'],function(bot,message) {
-    console.log('you are hungry');
     var menus = [
       "カレー","そば","うどん","ステーキ","パスタ","寿司","中華","ラーメン","とんかつ",
       "親子丼","牛丼","定食","ピザ","ハンバーグ","焼魚","唐揚げ","チャーハン","餃子","オムライス",
